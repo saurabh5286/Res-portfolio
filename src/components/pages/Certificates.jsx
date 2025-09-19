7e7c2dccf83f8a0220d8a507c05452e8a12c0ec2
Saurabh Hardel <saurabhhardel5286@gmail.com>
2025-09-19 11:07:16 +0530

    Certificates.jsx

--- src/components/pages/Certificates.jsx
+++ src/components/pages/Certificates.jsx
@@ -5,14 +5,14 @@ function Certificates() {
     {
       title: "Programming in JAVA",
       description: "mindluster",
-      imgSrc: "/JS.jpg",
+      imgSrc: "/Java.jpg",
       imgAlt: "Java Certificate",
       id: "java-cert",
     },
     {
       title: "JavaScript Fundamentals",
       description: "GreatStack",
-      imgSrc: "/JavaScript.jpg",
+      imgSrc: "/JS.jpg",
       imgAlt: "JavaScript Certificate",
       id: "javascript-cert",
     },
